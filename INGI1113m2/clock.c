62312e0daea020c615a7fb348022919a8bee66ee
Anthony Gégo <anthony@anthony-laptop>
2013-10-20 17:46:06 +0200

    Tab -> Espaces

--- INGI1113m2/clock.c
+++ INGI1113m2/clock.c
@@ -29,7 +29,7 @@ void HighISR(void) interrupt 1
 {
     if(INTCONbits.T0IF)
     {
-		// clear timer0 overflow bit and increment globalcount
+        // clear timer0 overflow bit and increment globalcount
         INTCONbits.T0IF=0;
         globalcount++;
         if(globalcount==8272800) globalcount = 0;
@@ -45,38 +45,38 @@ void HighISR(void) interrupt 1
  */
 void setMode(int new_mode)
 {
-	long seconds;
-	
-	if(new_mode != mode)
-	{
-		if(new_mode == 0)
-		{
-			mode = 0;
-			LATJbits.LATJ0=0;
-		}
-		else if(new_mode == 1)
-		{
-			mode = 1;
-			position = 0;
-			seconds = alarmtime == -1 ? 0 : alarmtime;
-			editValues[2] = seconds%60;
-			editValues[1] = (seconds/60)%60;
-			editValues[0] = (seconds/60/60)%24;
-		}
-		else if(new_mode == 2)
-		{
-			mode = 2;
-			position = 0;
-			seconds = globalcount/TIMER0_OVERFLOW_PER_SECOND;
-			editValues[2] = seconds%60;
-			editValues[1] = (seconds/60)%60;
-			editValues[0] = (seconds/60/60)%24;
-		}
-		else
-		{
-			mode = 3;
-		}
-	}
+    long seconds;
+    
+    if(new_mode != mode)
+    {
+        if(new_mode == 0)
+        {
+            mode = 0;
+            LATJbits.LATJ0=0;
+        }
+        else if(new_mode == 1)
+        {
+            mode = 1;
+            position = 0;
+            seconds = alarmtime == -1 ? 0 : alarmtime;
+            editValues[2] = seconds%60;
+            editValues[1] = (seconds/60)%60;
+            editValues[0] = (seconds/60/60)%24;
+        }
+        else if(new_mode == 2)
+        {
+            mode = 2;
+            position = 0;
+            seconds = globalcount/TIMER0_OVERFLOW_PER_SECOND;
+            editValues[2] = seconds%60;
+            editValues[1] = (seconds/60)%60;
+            editValues[0] = (seconds/60/60)%24;
+        }
+        else
+        {
+            mode = 3;
+        }
+    }
 }
 
 /*
@@ -88,37 +88,37 @@ void updateMode(int button)
     
     if(mode == 0)
     {
-		// Switching to edit alarm/clock mode
+        // Switching to edit alarm/clock mode
         setMode(button);
     }
     else if(mode == 1 || mode == 2)
     {
-		// Move the cursor to the next position
-		if(button == 1)
-			editValues[position] = (editValues[position] + 1) % ((position == 0) ? 24 : 60);
-		else
-		{
-			if(position == 2)
-			{
-				// Values are all edited, resume to mode 0
-				seconds = editValues[0]*60*60 + editValues[1]*60+ editValues[2];
-				if(mode == 2)
-					// Save new time
-					globalcount = seconds*TIMER0_OVERFLOW_PER_SECOND;
-				else
-					// Save new alarm
-					alarmtime = seconds;
-				setMode(0);
-			}
-			else
-				// Move the cursor
-				position++;
-		}
-		
-	}
+        // Move the cursor to the next position
+        if(button == 1)
+            editValues[position] = (editValues[position] + 1) % ((position == 0) ? 24 : 60);
+        else
+        {
+            if(position == 2)
+            {
+                // Values are all edited, resume to mode 0
+                seconds = editValues[0]*60*60 + editValues[1]*60+ editValues[2];
+                if(mode == 2)
+                    // Save new time
+                    globalcount = seconds*TIMER0_OVERFLOW_PER_SECOND;
+                else
+                    // Save new alarm
+                    alarmtime = seconds;
+                setMode(0);
+            }
+            else
+                // Move the cursor
+                position++;
+        }
+        
+    }
     else
     {
-		alarmtime = -1;
+        alarmtime = -1;
         setMode(0);
     }
 }
@@ -143,9 +143,9 @@ void LowISR(void) interrupt 2
 
 void activateTimer()
 {
-	T0CONbits.T08BIT=0; // use timer0 16-bit counter
-	T0CONbits.T0CS=0; // use timer0 instruction cycle clock
-	T0CONbits.PSA=1; // disable timer0 prescaler
+    T0CONbits.T08BIT=0; // use timer0 16-bit counter
+    T0CONbits.T0CS=0; // use timer0 instruction cycle clock
+    T0CONbits.PSA=1; // disable timer0 prescaler
     T0CONbits.T0PS0=0;
     T0CONbits.T0PS1=0;
     T0CONbits.T0PS2=0; //set prescaler at 1/64.
@@ -157,8 +157,8 @@ void activateTimer()
 // initialize board
 void init_board(void)
 {
-	TRISJbits.TRISJ0    = 0;   // configure PORTJ0 for output (LED)
-	TRISJbits.TRISJ1    = 0;   // configure PORTJ1 for output (LED)
+    TRISJbits.TRISJ0    = 0;   // configure PORTJ0 for output (LED)
+    TRISJbits.TRISJ1    = 0;   // configure PORTJ1 for output (LED)
     TRISJbits.TRISJ2    = 0;   // configure PORTJ1 for output (LED)
     
     RCONbits.IPEN       = 1;
@@ -177,7 +177,7 @@ void init_board(void)
     INTCON2bits.INT3IP  = 0;   //INT3 low priority
     
     LATJbits.LATJ0      = 0; // switch LED off
-	LATJbits.LATJ1      = 0; // switch LED off
+    LATJbits.LATJ1      = 0; // switch LED off
     LATJbits.LATJ2      = 0; // switch LED off
 }
 
@@ -204,41 +204,41 @@ void DisplayTime(BYTE pos)
     long seconds = globalcount/TIMER0_OVERFLOW_PER_SECOND;
     if(mode==0 || mode == 3)
     {
-		ts = seconds%60;
-		tm = (seconds/60)%60;
-		th = (seconds/60/60)%24;
-	}
-	else
-	{
-		ts = editValues[2];
-		tm = editValues[1];
-		th = editValues[0];
-		
-	}
+        ts = seconds%60;
+        tm = (seconds/60)%60;
+        th = (seconds/60/60)%24;
+    }
+    else
+    {
+        ts = editValues[2];
+        tm = editValues[1];
+        th = editValues[0];
+        
+    }
     DisplayTimeNumber(pos+0,th);
     LCDText[pos+2]=':';
     DisplayTimeNumber(pos+3,tm);
     LCDText[pos+5]=':';
     DisplayTimeNumber(pos+6,ts);
     if((mode == 1 || mode==2) && seconds % 2 == 0)
-	{
-		LCDText[pos+position*3]=' ';
-		LCDText[pos+position*3+1]=' ';
-	}
+    {
+        LCDText[pos+position*3]=' ';
+        LCDText[pos+position*3+1]=' ';
+    }
     LCDUpdate();
 }
 
 void main()
 {
-	// Variables to compute actual time
-	long actual_time;
+    // Variables to compute actual time
+    long actual_time;
     long prev_actual_time = -1;
     
     // Variables to compute edition time
     long edit_time;
     long prev_edit_time = -1;
     
-	init_board();
+    init_board();
     
     globalcount = 8271000; // tick count = 0
     alarmtime = -1; // alarm is not set
@@ -249,8 +249,8 @@ void main()
     
     while(1)
     {
-		// Computes actual/edit time
-		actual_time = globalcount/TIMER0_OVERFLOW_PER_SECOND;
+        // Computes actual/edit time
+        actual_time = globalcount/TIMER0_OVERFLOW_PER_SECOND;
         edit_time = editValues[0]*60*60+editValues[1]*60+editValues[2];
         
         // If actual/edit time has changed
@@ -262,35 +262,35 @@ void main()
             DisplayTime(4);
             
             // Check if alarm is on
-			if(mode==3)
-			{
-				LATJbits.LATJ0^=1; // led blinks
-				// If alarm has been on for 30 seconds, switch it off
-				if(actual_time==alarmtime+30)
-				{
-					alarmtime = -1;
-					setMode(0);
-				}
-			}
-			else if(actual_time==alarmtime) // triggers alarm
-				setMode(3);
-				
-			// Displaying mode
+            if(mode==3)
+            {
+                LATJbits.LATJ0^=1; // led blinks
+                // If alarm has been on for 30 seconds, switch it off
+                if(actual_time==alarmtime+30)
+                {
+                    alarmtime = -1;
+                    setMode(0);
+                }
+            }
+            else if(actual_time==alarmtime) // triggers alarm
+                setMode(3);
+                
+            // Displaying mode
             switch(mode)
             {
-				case 0:
-					DisplayString(16, "               ");
-					break;
-				case 1:
+                case 0:
+                    DisplayString(16, "               ");
+                    break;
+                case 1:
                     DisplayString(16, "Definir Alarme");
                     break;
                 case 2:
                     DisplayString(16, "Definir Heure");
                     break;
-				case 3:
-					DisplayString(16, "ALARME! ALARME!");
-					break;
-			}
+                case 3:
+                    DisplayString(16, "ALARME! ALARME!");
+                    break;
+            }
         }
     }
 }
--- INGI1113m2/test.c
+++ INGI1113m2/test.c
@@ -32,8 +32,8 @@ void HighISR(void) interrupt 1
 // initialize board
 void init_board(void)
 {
-	TRISJbits.TRISJ0    = 0;   // configure PORTJ0 for output (LED)
-	TRISJbits.TRISJ1    = 0;   // configure PORTJ1 for output (LED)
+    TRISJbits.TRISJ0    = 0;   // configure PORTJ0 for output (LED)
+    TRISJbits.TRISJ1    = 0;   // configure PORTJ1 for output (LED)
     TRISJbits.TRISJ2    = 0;   // configure PORTJ1 for output (LED)
     
     RCONbits.IPEN       = 1;
@@ -48,9 +48,9 @@ void init_board(void)
 //Timer 0
 void activateTimer0()
 {
-	T0CONbits.T08BIT=0; // use timer0 16-bit counter
-	T0CONbits.T0CS=0; // use timer0 instruction cycle clock
-	T0CONbits.PSA=1; // disable timer0 prescaler
+    T0CONbits.T08BIT=0; // use timer0 16-bit counter
+    T0CONbits.T0CS=0; // use timer0 instruction cycle clock
+    T0CONbits.PSA=1; // disable timer0 prescaler
     T0CONbits.T0PS0=0;
     T0CONbits.T0PS1=0;
     T0CONbits.T0PS2=0; //set prescaler at 1/64.
@@ -62,7 +62,7 @@ void activateTimer0()
 //Timer 1
 void activateTimer1()
 {
-	T1CONbits.RD16=0; //write 8 bits
+    T1CONbits.RD16=0; //write 8 bits
     T1CONbits.T1RUN=0; //use cristal
     T1CONbits.T1CKPS0=1;
     T1CONbits.T1CKPS1=1; //1:8 prescale (16sec for 1 overflow)
@@ -79,8 +79,8 @@ void main(void)
 {
     init_board();
     
-	LATJbits.LATJ0=0; // switch LED off
-	LATJbits.LATJ1=0; // switch LED off
+    LATJbits.LATJ0=0; // switch LED off
+    LATJbits.LATJ1=0; // switch LED off
     LATJbits.LATJ2=0; // switch LED off
     
     timer0count = 0;  // tick count = 0
